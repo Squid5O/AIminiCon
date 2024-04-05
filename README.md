3f75c8b56df1b9d20f2d5bf64983b620506f2e93
Squid O <5jing235@naver.com>
2024-04-05 11:52:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AIminiCon
+ aweg
---
+++ Source/AIminiCon/AIminiCon.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AIminiCon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIminiCon, "AIminiCon" );
+ 
